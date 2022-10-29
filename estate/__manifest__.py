0220e74bc7d695e08dfb157c441466c7eea4f81a
bushmusi <bushera.mestofa@gmail.com>
2022-10-29 06:10:56 +0300

    init commit

---
+++ estate/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Real Estate Company',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/test_view.xml',
+        'views/test_menu.xml',
+        'views/estate_property_views.xml',
+        'views/estate_menu.xml',
+    ],
+    'sequence': 0,
+}
---
+++ estate/models/test_model.py
@@ -0,0 +1,11 @@
+from typing_extensions import Required
+from pkg_resources import require
+from traitlets import default
+from odoo import models, fields
+
+class TestModel(models.Model):
+    _name = "test.model"
+    _description = "Test Model for checking model"
+
+    tester_name = fields.Text(default="Unknown")
+    last_seen = fields.Date("Last Seen", default=lambda self: fields.Datetime.now())
