3d17bec99b5a0d94fb8b4f37b1c2cccd9531e4fa
bushmusi <bushera.mestofa@gmail.com>
2022-11-07 10:04:31 +0300

    add constraint and color

--- property/models/estate_property_tag.py
+++ property/models/estate_property_tag.py
@@ -3,5 +3,10 @@ from odoo import models, fields
 class EstatePropertyTag(models.Model):
     _name = "estate.property.tag"
     _description="estate.property.tag"
+    _order = 'name asc'
 
     name = fields.Char()
+    color = fields.Integer(string="Color")
+    _sql_constraints = [
+        ('check_name', 'UNIQUE(name)', 'A tag must be unique!'),
+    ]
