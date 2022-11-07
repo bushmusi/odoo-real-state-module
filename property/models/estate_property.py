b255901a5c7d2943988c3039b8526d923a2c8ec6
bushmusi <bushera.mestofa@gmail.com>
2022-11-07 10:06:11 +0300

    Add validation

--- property/models/estate_property.py
+++ property/models/estate_property.py
@@ -1,9 +1,14 @@
-from odoo import models, fields
+from odoo import models, fields, api, _
 from datetime import timedelta
+from odoo.tools.float_utils import float_compare, float_is_zero
+from odoo.exceptions import UserError, ValidationError
+import logging
+_logger = logging.getLogger(__name__)
 
 class EstateProperty(models.Model):
     _name = 'estate.property'
     _description = 'This the main model for estate property'
+    _order = 'sequence, name '
 
     name = fields.Char(required=True)
     description = fields.Text()
@@ -35,12 +40,80 @@ class EstateProperty(models.Model):
         default=True
     )
     state = fields.Selection(
-        selection=[('New','New'),('Offer Received', 'Offer Received'),('offer accepted','offer accepted'),('Sold','Sold'),('Canceled','Canceled')],
+        selection=[('New','New'),('Offer Received', 'Offer Received'),('Offer accepted','Offer accepted'),('Sold','Sold'),('Canceled','Canceled')],
         required=True,
         copy=False,
         default="New"
     )
+    property_type_id = fields.Many2one('estate.property.type', string="Type")
     buyer_id = fields.Many2one("res.partner", string="Buyer")
     salesperson_id = fields.Many2one('res.users', string='Salesperson', index=True, tracking=True, default=lambda self: self.env.user)
     tag_ids = fields.Many2many("estate.property.tag", string="tages")
-    offer_ids = fields.Many2many("estate.property.offer", string="Offers")
+    offer_ids = fields.One2many("estate.property.offer", "property_id", string="Offers")
+    total_area = fields.Float(compute = "_compute_total_area")
+    best_price = fields.Float(compute = "_compute_best_price")
+    sequence = fields.Integer('Sequence', default=1, help="Used to order stages. Lower is better.")
+
+    _sql_constraints = [
+        ('check_expected_price', 'CHECK(expected_price >= 0)',
+         'Expected price should be more than 1.')
+    ]
+    
+    _sql_constraints = [
+        ('check_selling_price', 'CHECK(selling_price >= 0)',
+         'Selling price should be more than 1.')
+    ]
+
+
+
+    @api.depends('living_area', 'garden_area')
+    def _compute_total_area(self):
+        self.total_area = 0
+        for record in self:
+            record.total_area = record.living_area + record.garden_area
+    
+    @api.depends('offer_ids.price')
+    def _compute_best_price(self):
+        self.best_price = 0
+        for line in self:
+            if len(line.offer_ids) > 0:
+                line.best_price = max(line.offer_ids.mapped('price'))
+
+    @api.onchange('garden')
+    def _onchange_garden(self):
+        if self.garden:
+            self.garden_area = 10
+            self.graden_orentation = 'North'
+        else:
+            self.garden_area = 0
+            self.graden_orentation = None
+            # return {'warning': {
+            #     'title': _("Warning"),
+            #     'message': ('This option is not supported for Authorize.net')}}
+    def sell_property(self):
+        _logger.info('current property from parent: %s', self.name)
+        for record in self:
+            if record.state == 'Canceled':
+                raise UserError(_('Cancelled property can\'t be sold'))
+            # record.state = 'Sold'
+            return True
+    def cancel_property(self):
+        for record in self:
+            if record.state == 'Sold':
+                raise UserError(_('Sold property can\'t be cancelled'))
+            record.state = 'Sold'
+            return True
+    @api.constrains('selling_price')
+    def _check_selling_price(self):
+        for line in self:
+            min_price = 0.9 * line.expected_price
+            if not float_is_zero(line.expected_price, precision_digits=2) and float_compare(min_price, line.selling_price, precision_digits=2) == 1:
+                raise ValidationError(_("Selling price should be 90% of expeceted price"))
+    
+    def unlink(self):
+        for line in self:
+            _logger.info('Current module is%s',(line.state not in ['New', 'Canceled']))
+            if line.state not in ['New', 'Canceled']:
+                raise ValidationError(_('Only new or canceled property can be deleted'))
+            self.offer_ids.unlink()
+        return super(EstateProperty, self).unlink()
