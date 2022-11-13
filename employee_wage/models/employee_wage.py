befc1b95efbc9f06656a7b5c8b0d34b047d49e66
bushmusi <bushera.mestofa@gmail.com>
2022-11-13 23:25:16 +0300

    Change api constraint to sql constraint

--- employee_wage/models/employee_wage.py
+++ employee_wage/models/employee_wage.py
@@ -1,11 +1,12 @@
 from odoo import models, fields, api, _
 from odoo.tools.float_utils import float_compare
-from odoo.exceptions import ValidationError
+from odoo.exceptions import ValidationError, UserError
 import logging
 _logger = logging.getLogger(__name__)
 
 class EmployeeWage(models.Model):
-    _name = 'employee.wage'
+    _name = 'hr.job'
+    _inherit = 'hr.job'
     _description = "Employee salary change logic design"
 
     min_wage = fields.Float(required=True, string="Minimum wage")
@@ -13,14 +14,18 @@ class EmployeeWage(models.Model):
         required=True,
         string="Maximum wage"
     )
-    company_id = fields.Many2one('res.company', 'Company', default=lambda self: self.env.company, required=True)
-    job_id = fields.Many2one('hr.job', 'Job Position', domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]")
 
     _sql_constraints = [
         ('job_id_unique', 'unique(job_id)', 'Can\'t be duplicate Job position!'),
         ('check_min_wage','CHECK(min_wage > 0)', 'Minimum wage should be greater than zero'),
         ('check_max_wage','CHECK(max_wage > 0)', 'maximum wage should be greater than zero'),
+        ('check_max_wage_greater_than_min_wage','CHECK(max_wage > min_wage)', 'maximum wage should be greater than zero'),
     ]
+
+    @api.constrains('selling_price')
+    def _check_max_wage(self):
+        if self.max_wage < self.min_wage:
+            raise UserError(_('Maximum wage should be greater than minimum wage'))
 
     @api.model
     def create(self, vals):
@@ -34,9 +39,7 @@ class WageControl(models.Model):
     
     @api.onchange('wage')
     def _compute_wage_validation(self):
-        """ We don't want the current user to be follower of all created job """
-        employee_wage = self.env['employee.wage'].search([('job_id', '=', self.job_id.id)])
-        _logger.info('employee wage: %s', employee_wage)
+        employee_wage = self.env['hr.job'].search([('id', '=', self.job_id.id)])
         if employee_wage:
             min_wage = int(employee_wage.min_wage)
             max_wage = int(employee_wage.max_wage)
@@ -46,4 +49,4 @@ class WageControl(models.Model):
             if not wage in range(min_wage, max_wage):
                 return {'warning': {
                     'title': _("Warning"),
-                    'message': ('For ',self.job_id.name,' salary should be range of ' , min_wage ,' to ' , max_wage)}}
+                    'message': ('For '+str(self.job_id.name)+' salary should be range of ' + str(min_wage) + ' to '+str(max_wage))}}
