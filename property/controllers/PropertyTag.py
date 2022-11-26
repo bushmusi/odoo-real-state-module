f8a9f22aac3c967e3c6a0245b29af38b690b6e9e
bushmusi <bushera.mestofa@gmail.com>
2022-11-26 09:52:22 +0300

    Make property-tag private

--- property/controllers/PropertyTag.py
+++ property/controllers/PropertyTag.py
@@ -6,7 +6,7 @@ import logging
 _logger = logging.getLogger(__name__)
 
 class PropertyTag(http.Controller):
-    @http.route('/property-tag', type="http", auth="public", website=True)
+    @http.route('/property-tag', type="http", auth="user", website=True)
     def property_tag_form(self, **kwargs):
         _logger.info('property tag page is called')
         tags = request.env['estate.property.tag'].sudo().search([])
@@ -15,7 +15,7 @@ class PropertyTag(http.Controller):
     @http.route('/create-property-tag', type="http", auth="public", website=True)
     def create_property_tag(self, **kwargs):
         _logger.info('Send data be like %s', kwargs)
-        result = request.env['estate.property.tag'].sudo().create({'name': kwargs.get('name')})
+        result = request.env['estate.property.tag'].sudo().create({'name': kwargs.get('name'), 'color': kwargs.get('color')})
         _logger.info('Create result be like %s', result)
         tags = request.env['estate.property.tag'].sudo().search([])
         return request.render('property.web_property_tag_form', {'tags': tags})
